0b6ab185efbcbd40fb0fe8126132d93b442f260e
Tobias <Tobias@Galois>
2014-09-17 00:15:19 -0600

    TXT

--- Proyecto1.2/src/src/Diccionario.java
+++ Proyecto1.2/src/src/Diccionario.java
@@ -1,6 +1,19 @@
-public class Diccionario {
+public class Diccionario extends Lista<Lista<String>> {
+
+	private Lista<Lista<String>> diccionario;
 
 	public Diccionario() {
+
+		creaDiccionario();
+
+	}
+
+	public void creaDiccionario() {
+
+		for (int i = 65; i < 91; i++) {
+			Lista<String> temp = new Lista<String>();
+			diccionario.addDataEnd(temp.creaLista((char) i));
+		}
 
 	}
 
--- Proyecto1.2/src/src/Main.java
+++ Proyecto1.2/src/src/Main.java
@@ -1,72 +1,36 @@
-
 public class Main {
-	
-	public static void main (String arg[]){
-		
-		
-		
-		char a=97;
-		Lista<Lista<String>> ABC= new Lista<Lista<String>>();
-		Lista<String> letraA ;
-		
-		
-		
-		Tablero tablero= new Tablero();
-		//tablero.setCas(new Casilla("g",4), 5, 5);
-		tablero.setCas(new Casilla(new Ficha(0,null)),0,0);
-		Casilla algo=tablero.getCas(5, 5);
-		
-		System.out.print(""+algo.getBonus());
-		
-		
-		
-		
-		
-		
-		
-		for (int i=65; i<91; i++){
-			
-			
-			ABC.addDataEnd(new Lista<String>());
-			//System.out.println(""+(char)i);
-		}
-		
-		
-		letraA = ABC.getObject(0);
-		
-		letraA= letraA.creaLista(a);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/**
-		for (int i=0; i<26; i++){
-			
-			 obj = ABC.getObject(i);
-			 temp= (Lista)obj;
-			 Alfabeto.addDataEnd(temp);
-			 
-			System.out.println(""+obj);
-		}
-		
-		**/
-		
-		
-		
-		
-		
-		
-		
-	}
 
-	
+	public static void main(String arg[]) {
+
+		// Diccionario dic = new Diccionario();
+
+		Lista<Lista<String>> dic = new Lista<Lista<String>>();
+		Lista<String> lista = new Lista<String>();
+
+		Lista<String> letraA;
+
+		lista.addDataBegin("Hola");
+
+		for (int i = 65; i < 91; i++) {
+
+			dic.addDataEnd(new Lista<String>());
+			// System.out.println(""+(char)i);
+		}
+
+		// dic.getObject(0).creaLista((char) 99);
+		letraA = dic.getObject(1);
+
+		letraA = letraA.creaLista((char) 98);
+
+		Tablero tablero = new Tablero();
+		// tablero.setCas(new Casilla("g",4), 5, 5);
+		tablero.setCas(new Casilla(new Ficha(0, null)), 0, 0);
+		Casilla algo = tablero.getCas(5, 5);
+
+		System.out.print("" + (char) 98);
+
+		System.out.println("" + letraA.getSize());
+
+	}
 
 }
