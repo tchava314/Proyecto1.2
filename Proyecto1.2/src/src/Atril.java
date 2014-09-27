46a78e3c2e933aa806021bdd6be8795b0b6b7221
Ariel <PC@TOSHIBA>
2014-09-26 22:16:05 -0600

    se agregan fichas de la bolsa al atril

--- Proyecto1.2/src/src/Atril.java
+++ Proyecto1.2/src/src/Atril.java
@@ -1,9 +1,22 @@
 public class Atril extends Tablero {
 
-	private final int sizeX = 1, sizeY = 7;
+	private final int sizeX = 7, sizeY = 1;
+	Tablero atril = new Tablero();
 
 	public Atril() { // constructor
 		super.newTab(sizeX, sizeY);
+
+	}
+
+	public Tablero repartirFichas(Lista<Ficha> bolsa) {
+		for (int i = 0; i < 7; i++) {
+			Ficha x = bolsa.EleAzar();
+			Casilla cas = new Casilla(x);
+
+			atril.getCas(i, 1).setFicha(x);
+
+		}
+		return atril;
 	}
 
 	public Casilla getFicha(int x, int y) {// metodo que devuelve una ficha en
